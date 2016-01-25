fe339b623d9753e7bfbe819f9d3a0737cbc322c1
Emre Yavuzyiğit <emre.yavuz.842@gmail.com>
2016-01-25 17:13:49 +0200

    Update main.php

--- src/main.php
+++ src/main.php
@@ -4,23 +4,37 @@ namespace ArrowParticle;
 
 use pocketmine\plugin\PluginBase;
 use pocketmine\event\Listener;
-use pocketmine\event\server\DataPacketReceiveEvent;
-use pocketmine\event\player\PlayerMoveEvent;
-use pocketmine\event\player\PlayerJoinEvent;
+use pocketmine\event\Entity\EntityShootBowEvent;
+use pocketmine\level\sound\FizzSound;
+use pocketmine\level\sound\ClickSound;
+use pocketmine\level\particle\DustParticle;
 use pocketmine\player;
 
 class Main extends PluginBase implements Listener{
 
-public function OnEnable(){
-  $this->getLogger()->info("§dArrowParticle §aPlugin Has been Enabled!"),
+        public function OnEnable(){
+            $this->getLogger()->info("§dArrowParticle §aPlugin Has been Enabled!"),
+        }
+        
+        public function OnDisable(){
+            $this->getLogger()->info("§dArrowParticle §cPlugin Has Benn Disabled!");
+        }
+        
+        public function onsb(EntityShootBowEvent $ev) {
+	              if ($ev->isCancelled()) return;
+	              $damage = $ev->getDamage();
+	              $player = $ev->getEntity();
+	              if (!($player instanceof Player)) return;
+	              
+	              for ($i=0;$i<$damage;$i++) {
+		                    $player->getLevel()->addParticle(new DustParticle(self::randVector($player),(mt_rand()/mt_getrandmax())*2));
+		                    $player->getLevel()->addSound(new ClickSound($player));
+	              }
+        }
+        
+        public function OnDeath(PlayerDeathEvent $event) {
+          $event->getlevel()->addSound(new FizzSound($player));
+          $event->sendMessage("§c Birisi öldü :( ?");
+        }
 }
-
-public function OnDisable(){
-  4this->getLogger()->info("§dArrowParticle §cPlugin Has Benn Disabled!");
-}
-
-public function OnShot(){
-  $this->getItem(OnUse) == 262 {
-    // to be countine..
-  }
-}
+// this plugin not tested!
