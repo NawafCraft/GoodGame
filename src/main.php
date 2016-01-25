4157b3f4e42d0efa422ce235a97a5b05e336ecfc
Emre Yavuzyiğit <emre.yavuz.842@gmail.com>
2016-01-25 16:12:54 +0200

    Update main.php

--- src/main.php
+++ src/main.php
@@ -12,3 +12,15 @@ use pocketmine\player;
 class Main extends PluginBase implements Listener{
 
 public function OnEnable(){
+  $this->getLogger()->info("§dArrowParticle §aPlugin Has been Enabled!"),
+}
+
+public function OnDisable(){
+  4this->getLogger()->info("§dArrowParticle §cPlugin Has Benn Disabled!");
+}
+
+public function OnShot(){
+  $this->getItem(OnUse) == 262 {
+    // to be countine..
+  }
+}
