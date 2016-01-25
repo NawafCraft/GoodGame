e3441007e823e1428ba303fbaca5f2906c76a244
Emre Yavuzyiğit <emre.yavuz.842@gmail.com>
2016-01-25 17:19:32 +0200

    Update main.php

--- src/main.php
+++ src/main.php
@@ -12,29 +12,29 @@ use pocketmine\player;
 
 class Main extends PluginBase implements Listener{
 
-        public function OnEnable(){
-            $this->getLogger()->info("§dArrowParticle §aPlugin Has been Enabled!"),
+        public function OnEnable() {
+                $this->getLogger()->info("§dArrowParticle §aPlugin Has been Enabled!");
         }
         
-        public function OnDisable(){
-            $this->getLogger()->info("§dArrowParticle §cPlugin Has Benn Disabled!");
+        public function OnDisable() {
+                $this->getLogger()->info("§dArrowParticle §cPlugin Has Benn Disabled!");
         }
         
         public function onsb(EntityShootBowEvent $ev) {
-	              if ($ev->isCancelled()) return;
-	              $damage = $ev->getDamage();
-	              $player = $ev->getEntity();
-	              if (!($player instanceof Player)) return;
+	        if ($ev->isCancelled()) return;
+	        $damage = $ev->getDamage();
+	        $player = $ev->getEntity();
+	        if (!($player instanceof Player)) return;
 	              
-	              for ($i=0;$i<$damage;$i++) {
-		                    $player->getLevel()->addParticle(new DustParticle(self::randVector($player),(mt_rand()/mt_getrandmax())*2));
-		                    $player->getLevel()->addSound(new ClickSound($player));
+	        for ($i=0;$i<$damage;$i++) {
+		        $player->getLevel()->addParticle(new DustParticle(self::randVector($player),(mt_rand()/mt_getrandmax())*2));
+		        $player->getLevel()->addSound(new ClickSound($player));
 	              }
         }
         
         public function OnDeath(PlayerDeathEvent $event) {
-          $event->getlevel()->addSound(new FizzSound($player));
-          $event->sendMessage("§c Birisi öldü :( ?");
+        	$event->getlevel()->addSound(new FizzSound($player));
+                $event->sendMessage("§c Birisi öldü :( ?");
         }
 }
 // this plugin not tested!
